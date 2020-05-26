aeaee974866147361b56c3803f58caddc0854dec
ronan98 <64504440+ronan98@users.noreply.github.com>
2020-05-26 11:40:26 -0700

    Changed if statement to when statement in function

--- p3.kt
+++ p3.kt
@@ -1,6 +1,7 @@
 fun main() {
-    var cardPoints = 7_000
-
+    print("How many cardPoints do you have? ")
+    var cardPoints = Integer.valueOf(readLine())
+/*
     // TODO: replace this if with a when
     // try to use  ranges!
 
@@ -12,6 +13,17 @@ fun main() {
         "gold"
     } else {
         "platinum"
+    }
+
+ */
+    val cardLevel: String
+
+    when(cardPoints) {
+        in Int.MIN_VALUE..-1 -> cardLevel = "broke"
+        in 0..999 -> cardLevel = "pearl"
+        in 1000..4999 -> cardLevel = "silver"
+        in 5000..9999 -> cardLevel = "gold"
+        else -> cardLevel = "platinum"
     }
 
     val plural = if(cardPoints > 1 || cardPoints == 0) "s" else ""
